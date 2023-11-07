a8241418d0b43d5bb9e5f2e78b4fafadecf632a9
raindmj <lorrainedmj@gmail.com>
2023-11-07 12:04:20 -0600

    study page functional

--- src/Study/StudyCard.js
+++ src/Study/StudyCard.js
@@ -1,23 +1,87 @@
 import React, { useState } from "react";
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Switch, Link, useHistory } from "react-router-dom";
 
 function StudyCard({ currentDeck }) {
+  // console.log(currentDeck)
   const [isFlipped, setIsFlipped] = useState(false);
+  const [index, setIndex] = useState(0);
+
+  const cards = currentDeck.cards;
+  // console.log(cards);
+
+  const history = useHistory();
+
+  function handleBack() {
+    if (index > 0) {
+      setIndex(index - 1);
+    }
+  }
 
   function handleFlip() {
     setIsFlipped(!isFlipped);
   }
 
-  const cards = currentDeck.cards;
-  console.log(cards)
+  function handleNext() {
+    if (index + 1 < cards.length) {
+      setIndex(index + 1);
+    }
+    if (isFlipped === false) {
+      setIsFlipped(true);
+    }
+  }
+
+  function handleRestart() {
+    if (window.confirm("Restart cards? Click 'cancel' to return to the home page.")) {
+      setIndex(0);
+    } else {
+      history.push("/");
+    }
+  }
 
   return (
     <div>
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">Card </h5>
-          <p className="card-text">Text</p>
-          <button type="button" className="btn btn-secondary" onClick={handleFlip}>Flip</button>
+          <h5 className="card-title">
+            Card {index + 1} of {cards.length}{" "}
+          </h5>
+          <p className="card-text">
+            {isFlipped ? cards[index].front : cards[index].back}
+          </p>
+          {index > 0 && (
+            <button
+              type="button"
+              className="btn btn-dark mr-2"
+              onClick={handleBack}
+            >
+              Back
+            </button>
+          )}
+          <button
+            type="button"
+            className="btn btn-secondary mr-2"
+            onClick={handleFlip}
+          >
+            {isFlipped ? "Flip to Back" : "Flip to Front"}
+          </button>
+          {index + 1 < cards.length && (
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          )}
+          {index === (cards.length - 1) && (
+            <button
+              type="button"
+              className="btn btn-success"
+              onClick={handleRestart}
+            >
+              Restart Deck
+            </button>
+          )}
         </div>
       </div>
     </div>
